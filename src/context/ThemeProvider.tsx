8f5e3736ff32b17a65ba2a1dd7688cb794d8fabb
nanonoir <nahuelnoir@live.com.ar>
2025-08-27 17:52:10 -0300

    feat(theme): Implement theme context with Os preference detection

---
+++ src/context/ThemeProvider.tsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from 'react';
+import { type Theme, type ThemeContextType, type ThemeProviderProps } from '../types'
+
+export const ThemeContext = createContext<ThemeContextType>({} as ThemeContextType);
+
+export const ThemeProvider: React.FC<ThemeProviderProps> = ({ children }) => {
+    const [theme, setTheme] = useState<Theme>(() => {
+        const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        return prefersDarkMode ? 'dark' : 'light';
+        
+    });
+
+    useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const handleChange = (e: MediaQueryListEvent) => {
+        setTheme(e.matches ? theme);
+    };
+
+    mediaQuery.addEventListener('change', handleChange);
+    
+    return () => {
+        mediaQuery.removeEventListener('change', handleChange);
+    };
+
+    }, []);
+
+    const toggleTheme = () => {
+        setTheme(prevTheme => (prevTheme === 'light' ? 'dark' : 'light'));
+    };
+
+    return (
+        <ThemeContext.Provider value={{ theme, toggleTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
